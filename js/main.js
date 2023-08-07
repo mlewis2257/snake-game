97afff701f2ee94be436027abddfc0eda1f83c6f
mlewis2257 <mlang.lewis@gmail.com>
2023-08-07 15:05:01 -0700

    grid pattern added

--- js/main.js
+++ js/main.js
@@ -1,15 +1,27 @@
 /*----- constants -----*/
+const gridDiv = document.getElementById("game_grid");
 
 /*----- state variables -----*/
 let board, score, snake, food, direction;
 /*----- cached elements  -----*/
 
+// const gridDiv = document.getElementById("game_grid");
 /*----- event listeners -----*/
 
 /*----- functions -----*/
+const renderGrid = () => {
+  for (let i = 0; i < 440; i++) {
+    let gridPattern = document.createElement("div");
+    gridPattern.classList.add("gridPattern");
+    gridDiv.appendChild(gridPattern);
+  }
+};
 
+const render = () => {
+  renderGrid();
+};
 const init = () => {
-  snake = [[0, 0]];
+  render();
 };
 
 init();
