078565c5c43485d5148c09c4670ab131702d30d5
mlewis2257 <mlang.lewis@gmail.com>
2023-08-09 21:51:46 -0700

    Updated the class and added new food class

--- js/main.js
+++ js/main.js
@@ -1,36 +1,82 @@
 /*----- constants -----*/
 const gridWidth = 30;
 const gridHeight = 30;
+const SNAKE_SPEED = 1;
 
 /*----- state variables -----*/
 let score, snake, food, direction, board;
 let currentIdx = 0;
 let foodIdx = 0;
+let lastRenderTime = 0;
 // Classes
 class Snake {
   constructor() {
     this.direction = "up";
-    this.body = {
-      x: 14,
-      y: 14,
-    };
-    this.head = this.body.x;
-    this.tail = this.body.y;
-    this.size = 1;
+    this.body = [
+      {
+        x: 14,
+        y: 14,
+      },
+    ];
+    this.head = { ...this.body[0] };
+    this.tail = { ...this.body[this.body.length - 1] };
+    this.length = 1;
+    this.color = "green";
   }
 
-  changeDirection(direction) {
-    this.direction = direction;
-    console.log(direction);
+  changeDirection(newDirection) {
+    if (
+      (newDirection === "left" && this.direction !== "right") ||
+      (newDirection === "right" && this.direction !== "left") ||
+      (newDirection === "down" && this.direction !== "up") ||
+      (newDirection === "up" && this.direction !== "down")
+    ) {
+      this.direction = newDirection;
+      return newDirection;
+    }
+  }
+  move() {
+    let newHead = this.head;
+
+    switch (this.direction) {
+      case "left":
+        newHead.x -= 1;
+        break;
+      case "right":
+        newHead.x += 1;
+        break;
+      case "up":
+        newHead.y -= 1;
+        break;
+      case "down":
+        newHead.y += 1;
+        break;
+    }
+    return newHead;
+  }
+  eatFood() {}
+  grow() {
+    this.body.push(this.tail);
+    this.length++;
   }
 }
 
 /*----- cached elements  -----*/
 const gridDiv = document.getElementById("board");
-// const gridDiv = document.getElementById("game_grid");
+
 /*----- event listeners -----*/
 
 /*----- functions -----*/
+const update = () => {};
+const main = (currentTime) => {
+  window.requestAnimationFrame(main);
+  const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
+  if (secondsSinceLastRender < 1 / SNAKE_SPEED) return;
+  console.log("render");
+  lastRenderTime = currentTime;
+};
+
+// window.requestAnimationFrame(main);
 
 const init = () => {
   score = 0;
@@ -53,13 +99,10 @@ const buildBoard = () => {
 };
 
 const renderGrid = () => {
-  for (let i = 0; i < 600; i++) {
-    let gridPattern = document.createElement("div");
-    gridPattern.classList.add("gridPattern");
-    gridDiv.appendChild(gridPattern);
-    gridPattern.style.width = `${gridWidth}px`;
-    gridPattern.style.height = `${gridHeight}px`;
-  }
+  board.forEach((colArr, colIdx) => {
+    console.log(colArr, colIdx);
+    colArr.forEach((cell, rowIdx) => {});
+  });
 };
 
 const renderSnake = () => {};
@@ -75,10 +118,6 @@ const moveSnake = () => {
   // Tail will equal snake.pop()
   // Snake
 };
-// const iterateDivs = () => {
-//   const childDivs = document.querySelectorAll("#game_grid > div");
-//   console.log(childDivs);
-// };
 
 const render = () => {
   renderGrid();
